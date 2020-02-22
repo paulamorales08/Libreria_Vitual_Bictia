18dbc2e91d9cbe17a84167242dcf4663f2b5bd7b
Bryam <bapinerosh@itc.edu.co>
2020-02-21 22:23:51 -0500

    Se agrego interfaz grafica de cada seccion

--- categorias/pruebaIndex.php
+++ categorias/pruebaIndex.php
@@ -4,15 +4,17 @@ include_once('../moduloImagenes/claseImagen.php');
 $imagen = new Imagen();
 $categoria = new Categoria();
 
+
 if (isset($_GET) && !empty($_GET)) {
     $consultaLibro = $_GET['idCategoria'];
     $todosLibros = $categoria->filtroLibrosCategoria($consultaLibro);
-   
 } else {
     echo "No se encontro";
 }
+$nombreCat = $categoria->obtenerCategoria($consultaLibro);
 
 ?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,23 +22,46 @@ if (isset($_GET) && !empty($_GET)) {
     <title>Document</title>
 </head>
 
-<h1>Prueba</h1>
+<body>
 
-<?php
+    <div class="container text-center p-4 shadow p-3 mb-5 bg-white rounded">
 
-    while ($libroRecorrido = mysqli_fetch_object($todosLibros)) {
-        echo $libroRecorrido->nombreLibro;
-        echo $libroRecorrido->descripcion;
-        echo $libroRecorrido->idLibro;
-       // $primeraImagenLibro= $imagen->obtenerPrimeraImagen($libroRecorrido->idLibro);
-       // echo "<td> <img src='$imagen->root/moduloImagenes/imagenesLibros/$primeraImagenLibro->urlImagen' class='d-block w-100' alt='$primeraImagenLibro->nombreImagen' width='300px'/></td>";
-    }
+        <?php
+           echo" <h3 class='font-weight-light pb-2'>Sección: <span class='badge badge-secondary'>$nombreCat->nombreCategoria</span></h3>";
+        ?>
 
-?>
+        <div class="d-flex flex-row bd-highlight mb-3 d-flex justify-content-center d-flex flex-wrap">
+            <?php
+            while ($libroRecorrido = mysqli_fetch_object($todosLibros)) {
+                echo "<div class='p-2 bd-highlight'>";
+                echo "<div class='card text-center shadow-sm p-3 mb-5 bg-white rounded' style='width: 18rem;'>";
+                echo " <img src='...' class='card-img-top' alt='Imagen-libro'>
+            <div class='card-body'>";
+                echo "<h5 class='card-title'>$libroRecorrido->nombreLibro</h5>";
+                echo "<p class='card-text text-justify'>$libroRecorrido->descripcion</p>";
+                echo "</div>";
+                echo "<ul class='list-group list-group-flush'>";
+                echo "<li class='list-group-item'>$libroRecorrido->fechaPublicacion</li>";
+                echo "<li class='list-group-item'>Precio: $$libroRecorrido->precio</li>";
+                echo "<li class='list-group-item'>Estado: $libroRecorrido->estado</li>";
+                echo "<li class='list-group-item'>idAutor: $libroRecorrido->idAutor</li>";
+                echo "<li class='list-group-item'>idEditorial: $libroRecorrido->idEditorial</li>";
+
+                echo " </ul>
+                        </div>";
+                echo "</br>";
+                echo "</div>";
+
+                $libroRecorrido->idLibro;
+                //$primeraImagenLibro= $imagen->obtenerPrimeraImagen($libroRecorrido->idLibro);
+                //echo "<td> <img src='$imagen->root/moduloImagenes/imagenesLibros/$primeraImagenLibro->urlImagen' class='d-block w-100' alt='$primeraImagenLibro->nombreImagen' width='300px'/></td>";
+            }
+            ?>
+        </div>
+    </div>
 
 
-
-
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
