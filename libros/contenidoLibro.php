83aee2fbae1d6c58e0f82d73023b30db6521329b
Redvers Escobar Vaquer <redversescobar@gmail.com>
2020-02-21 21:08:31 -0500

    Página de detalle de libro casi lista. :)

--- libros/contenidoLibro.php
+++ libros/contenidoLibro.php
@@ -3,21 +3,28 @@
         <?php
 
             include_once('../moduloImagenes/listadoImagenesLibro.php');
+            include_once('../moduloImagenes/claseImagen.php');
+            $libro = new Libro();
+            $idLibro = $_GET['idLibro'];
+
+            $datosDelLibro = $libro->obtenerLibro($idLibro);
 
         ?>
     </div>
     <div class="datosLibro">
         <div class="nombreLibro">
         <h1>
-            Nombre del Libro
+            <?= $datosDelLibro->nombreLibro ?>
         </h1>
         </div>
-        <div class="nombreAutor">Autor</div>
-        <div class="publicacion">Año de Publicación - Categoría:</div>
-        <div class="descripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam voluptate rerum aspernatur, magnam similique temporibus dolorum voluptatem ea ad ex reprehenderit tenetur eaque, possimus mollitia neque labore aut omnis quae.</div>
+        <div class="nombreAutor">Autor: <?= $datosDelLibro->idAutor ?></div>
+        <div class="publicacion">Año de Publicación <?= $datosDelLibro->fechaPublicacion ?> - Categoría: <?= $datosDelLibro->idCategoria ?></div>
+        <div class="descripcion">
+        <?= $datosDelLibro->descripcion ?>
+        </div>
 
-        <div class="valoracion">***** </div>
-        <div class="precio">$ 00.000</div>
+        <div class="valoracion"> <img src="../includes/imagenes/valoracion_cuatro.png" alt="Valoración"></div>
+        <div class="precio">$ <?= $datosDelLibro->precio ?></div>
 
 
 
